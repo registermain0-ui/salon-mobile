d4898e1f84ce8ed65f7312a608decabec120c59a
registermain0-ui <registermain0@gmail.com>
2026-07-02 06:00:28 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+/* サロン受付 M-V1 Service Worker: 完全オフライン動作用 */
+const CACHE = "este-mobile-mv1-1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-180.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+// キャッシュ優先(オフライン最優先)。ネットに繋がった時は裏で更新。
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(cached => {
+      const fetched = fetch(e.request).then(res => {
+        if (res && res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fetched;
+    })
+  );
+});
