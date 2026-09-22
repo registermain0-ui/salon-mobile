90f7b7c4074093e510efdd98b5e12aac472a6d62
registermain0-ui <registermain0@gmail.com>
2026-09-23 04:16:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* サロン受付 M-V12 Service Worker: 完全オフライン動作用 */
-const CACHE = "este-mobile-mv12-1";
+/* サロン受付 M-V12.1 Service Worker: 完全オフライン動作用 */
+const CACHE = "este-mobile-mv12-2";
 const ASSETS = [
   "./",
   "./index.html",
